31e806abd38775953ec06790c8a102e68751ec2d
fgrullon <franfelix@gmail.com>
2017-08-29 11:45:46 -0400

    add data sanitation and finish the features

--- crimemap.py
+++ crimemap.py
@@ -3,43 +3,54 @@ from flask import Flask
 from flask import render_template
 from flask import request
 import json
+import datetime
+import dateparser
+import string
 
 
 app = Flask(__name__)
 DB = DBHelper()
 
+categories = ['mugging', 'break-in']
+
 @app.route("/")
-def home():
+def home(error_message=None):
     crimes = DB.get_all_crimes()
     crimes = json.dumps(crimes)
-    return render_template("home.html", crimes=crimes)
-
-@app.route("/add", methods=["POST"])
-def add():
-    try:
-	    data = request.form.get("userinput")
-	    DB.add_input(data)
-    except Exception as e:
-	print e
-    return home()
-
-@app.route("/clear")
-def clear():
-    try:
-	    DB.clear_all()
-    except Exception as e:
-	print e
-    return home()
+    return render_template("home.html", crimes=crimes, categories=categories, error_message=error_message)
 
 @app.route("/submitcrime", methods=['POST'])
 def submitcrime():
+    #catergory
     category = request.form.get("category")
-    date = request.form.get("date")
-    latitude = float(request.form.get("latitude"))
-    longitude = float(request.form.get("longitude"))
-    description = request.form.get("description")
+    if category not in categories:
+        return home()
+        #date
+    date = format_date(request.form.get("date"))
+    if not date:
+        return home("Invalid date. Please use yyyy-mm-dd format.")
+    #location    
+    try:
+        latitude = float(request.form.get("latitude"))
+        longitude = float(request.form.get("longitude"))
+    except ValueError:
+        return home()
+    #description
+    description = sanitize_string(request.form.get("description"))
+    #send data to db
     DB.add_crime(category, date, latitude, longitude, description)
     return home()
+
+def format_date(userdate):
+    date = dateparser.parse(userdate)
+    try:
+        return datetime.datetime.strftime(date, "%Y-%m-%d")
+    except:
+        return None
+
+def sanitize_string(userinput):
+    whitelist = string.letters + string.digits + "!?$.,;:-'()&"
+    return filter(lambda x: z in whitelist, userinput)
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
